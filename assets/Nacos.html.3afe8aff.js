605a36fd8e404462f0863eabc73a9e0078043b1d
deantook <chenyunlong99@outlook.com>
2022-12-30 08:34:18 +0000

    Deploying to gh-pages from @ deantook/deantook.github.io@42297f7b69e8ecc8e3a36ab81c6c25f590f86257 🚀

--- assets/Nacos.html.3afe8aff.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as n,c as s,d as c}from"./app.50586386.js";const a={},i=c(`<p>Nacos</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git clone https://github.com/nacos-group/nacos-docker.git
-cd nacos-docker
-
-yum install -y docker-compose
-
-docker-compose -f example/standalone-derby.yaml up
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[i];function d(t,l){return n(),s("div",null,o)}const u=e(a,[["render",d],["__file","Nacos.html.vue"]]);export{u as default};
