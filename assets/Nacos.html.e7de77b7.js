45393a9813f56e0bbf557b8978e6c437ab9f4edc
stiflea <41585297+stiflea@users.noreply.github.com>
2023-03-24 07:35:34 +0000

    Deploying to gh-pages from @ stiflea/stiflea.github.io@fba2185a0adca0299a7d66f445173f6b40063cfe 🚀

---
+++ assets/Nacos.html.e7de77b7.js
@@ -0,0 +1,9 @@
+import{_ as e}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as n,c as s,a}from"./app.82a41abd.js";const i={},c=a(`<h1 id="nacos" tabindex="-1"><a class="header-anchor" href="#nacos" aria-hidden="true">#</a> Nacos</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>git clone https://github.com/nacos-group/nacos-docker.git
+cd nacos-docker
+
+
+## install
+yum install -y docker-compose
+
+docker-compose -f example/standalone-derby.yaml up
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),d=[c];function o(l,t){return n(),s("div",null,d)}const u=e(i,[["render",o],["__file","Nacos.html.vue"]]);export{u as default};
