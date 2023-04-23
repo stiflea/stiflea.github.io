1b4fab83c76f537a0d1eb14898f53b25bfc62a2f
stiflea <41585297+stiflea@users.noreply.github.com>
2023-04-23 05:35:13 +0000

    Deploying to gh-pages from @ stiflea/stiflea.github.io@4fc86b1be20ffd9711381c488297187c288b4df1 🚀

--- assets/Nginx.html.9a4bff49.js
+++ assets/Nginx.html.9a4bff49.js
@@ -1,4 +1,4 @@
-import{_ as n,o as i,c as s,a as e}from"./app.9d3f7451.js";const l={},a=e(`<h1 id="nginx-防火墙模块-静态编译安装-centos" tabindex="-1"><a class="header-anchor" href="#nginx-防火墙模块-静态编译安装-centos" aria-hidden="true">#</a> Nginx 防火墙模块 静态编译安装 CentOS</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 安装环境
+import{_ as n,o as i,c as s,a as e}from"./app.9b54de1f.js";const l={},a=e(`<h1 id="nginx-防火墙模块-静态编译安装-centos" tabindex="-1"><a class="header-anchor" href="#nginx-防火墙模块-静态编译安装-centos" aria-hidden="true">#</a> Nginx 防火墙模块 静态编译安装 CentOS</h1><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code># 安装环境
 yum install -y git gcc pcre pcre-devel zlib zlib-devel flex bison
 
 cd /usr/local/src
